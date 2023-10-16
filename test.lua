f9011a97e6874f2c501d74a017b52a1a6bb226a9
Kiano2 <82270273+Kiano2@users.noreply.github.com>
2023-10-16 17:22:20 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -6,20 +6,19 @@ local menu = {
 }
 
 local monitor = peripheral.find("monitor")
-local printer = peripheral.find("printer")
 monitor.setTextScale(0.5)  -- Kleinerer Text
 
--- Funktion zur Anzeige des Menüs
-function displayMenu()
+-- Funktion zur Anzeige des Menüs auf dem Monitor
+function displayMenuOnMonitor()
     monitor.clear()
     monitor.setCursorPos(1, 1)
     monitor.write("Willkommen bei McComputerCraft!")
-    
+
     for i, item in ipairs(menu) do
         monitor.setCursorPos(1, i * 2 + 1)
         monitor.write(i .. ") " .. item.name .. ": $" .. item.price)
     end
-    
+
     monitor.setCursorPos(1, #menu * 2 + 3)
     monitor.write("Geben Sie die Bestellnummer ein:")
 end
@@ -29,12 +28,12 @@ function takeOrder()
     local orderComplete = false
     local totalCost = 0
     local customerOrder = {}
-    
+
     while not orderComplete do
-        displayMenu()
+        displayMenuOnMonitor()
         monitor.setCursorPos(1, #menu * 2 + 4)
         local choice = tonumber(read())
-        
+
         if choice and choice >= 1 and choice <= #menu then
             local selectedItem = menu[choice]
             table.insert(customerOrder, selectedItem)
@@ -48,29 +47,17 @@ function takeOrder()
             sleep(2)  -- 2 Sekunden warten, um die Nachricht anzuzeigen
         end
     end
-    
-    -- Ausgabe der Bestellung auf den Drucker
-    printer.clear()
-    printer.setCursorPos(1, 1)
-    printer.write("McComputerCraft Bestellung:")
-    
-    for i, item in ipairs(customerOrder) do
-        printer.setCursorPos(1, i + 2)
-        printer.write(item.name .. ": $" .. item.price)
-    end
-    
-    printer.setCursorPos(1, #customerOrder + 3)
-    printer.write("Gesamtpreis: $" .. totalCost)
-    
+
+    -- Ausgabe der Bestellung auf dem Monitor
     monitor.clear()
     monitor.setCursorPos(1, 1)
     monitor.write("Vielen Dank für Ihre Bestellung!")
-    
+
     for i, item in ipairs(customerOrder) do
         monitor.setCursorPos(1, i * 2 + 1)
         monitor.write(item.name .. ": $" .. item.price)
     end
-    
+
     monitor.setCursorPos(1, #menu * 2 + 3)
     monitor.write("Gesamtpreis: $" .. totalCost)
     sleep(5) -- Warte 5 Sekunden, bevor der Bildschirm gelöscht wird
@@ -79,6 +66,6 @@ end
 
 -- Hauptprogramm
 while true do
-    displayMenu()
+    displayMenuOnMonitor()
     takeOrder()
 end
